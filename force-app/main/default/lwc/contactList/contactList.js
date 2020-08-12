668c880fa8e458a48ed7aecf3bffce1eadede3c7
Victor Alsheuski <alsheuskivictor@gmail.com>
2020-08-12 23:23:57 +0300

    delete contact by click button; modal with delete confirmation

--- force-app/main/default/lwc/contactList/contactList.js
+++ force-app/main/default/lwc/contactList/contactList.js
@@ -1,17 +1,21 @@
 import { LightningElement, api, wire, track } from 'lwc';
 import getContacts from '@salesforce/apex/ContactController.getContacts';
+import deleteContacts from '@salesforce/apex/ContactController.deleteContacts';
+import { ShowToastEvent } from "lightning/platformShowToastEvent";
 
 export default class ContactList extends LightningElement {
 
     @api searchName = '';
     @api columns;
-    @track contacts = [];
-    @track error;
-    
+    contacts = [];
+    error;
+    openconfirmation = false;
+    deleteContactId = 0;
+
     @wire(getContacts, {searchName: '$searchName'}) 
     wiredContacts({data, error}) {
        
-        if(data) {
+        if (data) {
             let resultData = [];
             for (let i=0; i<data.length; i++) {
                 let row = {};
@@ -30,4 +34,77 @@ export default class ContactList extends LightningElement {
         }
     }
 
+    handleRowAction(cmp, event, helper) {
+
+        const action = cmp.detail.action.name;
+        const contactId = cmp.detail.row.Id;
+        switch (action) {
+          case "delete":
+            // console.log("cmp.detail: " + JSON.stringify(cmp.detail));
+            console.log("handleRowAction delete contactId: " + contactId);
+            
+            this.deleteContactId = contactId;
+            this.showConfirmation();
+            break;
+          default:
+            console.log("no action");
+        }
+    }
+
+    showConfirmation() {
+
+        this.openconfirmation = true;
+    }
+    
+    closeConfirmation() {
+
+        console.log("ContactList closeConfirmation");
+        this.openconfirmation = false;
+    }
+
+    handleCloseConfirmation() {
+        
+        console.log("ContactList handleCloseConfirmation");
+        this.closeConfirmation();
+    }
+
+    handleDelete() {
+        try {
+            console.log("ContactList handleDelete");
+            let deleteContactIds = this.deleteContactId;
+            deleteContacts({deleteContactIds});
+            this.handleResponse("success", "Delete Contact");
+        } catch (error) {
+          console.error(error);
+          this.handleResponse("fail", "Delete Contact");
+        }
+        this.closeConfirmation();
+    }
+
+    handleResponse(type, message) {
+        let toastEvent = null;
+        switch (type) {
+          case "success":
+            toastEvent = new ShowToastEvent({
+              title: "Success operation.",
+              message: "Operation '"+message+"' executed successfully.",
+              variant: "success"
+            });
+            break;
+          case "fail":
+            toastEvent = new ShowToastEvent({
+              title: "Failed operation",
+              message: "Failed executed operation: '"+message+"'.",
+              variant: "error"
+            });
+            break;
+          default:
+            toastEvent = new ShowToastEvent({
+              title: "Unexpected operation.",
+              message: "Contact the administrator for more information."
+            });
+        }
+        this.dispatchEvent(toastEvent);
+      }
+    
 }
--- force-app/main/default/lwc/contactListForm/contactListForm.js
+++ force-app/main/default/lwc/contactListForm/contactListForm.js
@@ -38,12 +38,25 @@ const COLUMNS = [
         type: 'date',
         typeAttributes: { month:"2-digit", day:"2-digit", year:"numeric", hour:"2-digit", minute:"2-digit"},
     },
+    {
+        label: "Delete",
+        type: "button",
+        initialWidth: 120,
+        typeAttributes: {
+          label: { fieldName: "actionLabel" },
+          title: "Click to Delete",
+          name: "delete",
+          iconName: "utility:delete",
+          disabled: { fieldName: "actionDisabled" },
+          class: "btn_next"
+        }
+      }    
 ];
 
 export default class ContactListForm extends LightningElement {
 
     columns = COLUMNS;
-    @track searchName = '';
+    searchName = '';
 
     changedSearchName = '';
 
