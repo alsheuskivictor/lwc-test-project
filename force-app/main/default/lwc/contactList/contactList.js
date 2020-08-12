577a34bd047446c9ca134b62bc9a009cbd17e06b
Victor Alsheuski <alsheuskivictor@gmail.com>
2020-08-12 13:36:12 +0300

    display account name as link and redirect to account detail page by click

--- force-app/main/default/lwc/contactList/contactList.js
+++ force-app/main/default/lwc/contactList/contactList.js
@@ -1,11 +1,31 @@
-import { LightningElement, api, wire } from 'lwc';
+import { LightningElement, api, wire, track } from 'lwc';
 import getContacts from '@salesforce/apex/ContactController.getContacts';
 
 export default class ContactList extends LightningElement {
 
     @api columns;
-
-    @wire(getContacts)
-    contacts;
+    @track contacts = [];
+    @track error;
     
+    @wire(getContacts) 
+    wiredContacts({data, error}) {
+        if(data) {
+            let resultData = [];
+            for (let i=0; i<data.length; i++) {
+                let row = {};
+                Object.assign(row, data[i]);
+                if (row.hasOwnProperty('Account')) {
+                    Object.assign(row, {AccountNameLabel: row.Account.Name});
+                    Object.assign(row, {AccountNameUrl:'/lightning/r/Account/' +row.AccountId + '/view'});
+                }
+                resultData.push(row);
+            }
+            this.contacts = resultData;
+            this.error = undefined;
+        } else if (error) {
+            this.contacts = [];
+            this.error = error;
+        }
+    }
+
 }
