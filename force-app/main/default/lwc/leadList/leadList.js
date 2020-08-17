e1f0718cf575f0f38af9d79de3a78f634877cf67
Victor Alsheuski <alsheuskivictor@gmail.com>
2020-08-17 09:16:52 +0300

    lwc component for display list (name, title, phone) of leads

---
+++ force-app/main/default/lwc/leadList/leadList.js
@@ -0,0 +1,31 @@
+import { LightningElement, wire, track } from 'lwc';
+import getLeads from '@salesforce/apex/LeadController.getLeads';
+import NAME_FIELD from '@salesforce/schema/Lead.Name';
+import TITLE_FIELD from '@salesforce/schema/Lead.Title';
+import PHONE_FIELD from '@salesforce/schema/Lead.Phone';
+
+const COLUMNS = [
+    { 
+        label: 'Name', 
+        fieldName: NAME_FIELD.fieldApiName, 
+        type: 'text',
+    },
+    { 
+        label: 'Title', 
+        fieldName: TITLE_FIELD.fieldApiName, 
+        type: 'text',
+    },
+    { 
+        label: 'Phone', 
+        fieldName: PHONE_FIELD.fieldApiName, 
+        type: 'phone'
+    } 
+];
+
+export default class LeadList extends LightningElement {
+
+    columns = COLUMNS;
+
+    @wire(getLeads)
+    leads;
+}
