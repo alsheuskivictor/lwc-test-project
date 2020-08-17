6a2b3b0e9dd773223b26537c29259a3d9c6597d8
Victor Alsheuski <alsheuskivictor@gmail.com>
2020-08-17 14:35:59 +0300

    editable fields of lead table

--- force-app/main/default/lwc/leadList/leadList.js
+++ force-app/main/default/lwc/leadList/leadList.js
@@ -1,15 +1,14 @@
 import { LightningElement, wire, track } from 'lwc';
+import { updateRecord } from 'lightning/uiRecordApi';
+import { refreshApex } from '@salesforce/apex';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+
 import getLeads from '@salesforce/apex/LeadController.getLeads';
 import NAME_FIELD from '@salesforce/schema/Lead.Name';
 import TITLE_FIELD from '@salesforce/schema/Lead.Title';
 import PHONE_FIELD from '@salesforce/schema/Lead.Phone';
 
 const COLUMNS = [
-    // { 
-    //     label: 'Name', 
-    //     fieldName: NAME_FIELD.fieldApiName, 
-    //     type: 'text',
-    // },
     {
         label:'Name',
         fieldName:'NameUrl',
@@ -20,41 +19,73 @@ const COLUMNS = [
         label: 'Title', 
         fieldName: TITLE_FIELD.fieldApiName, 
         type: 'text',
+        editable : 'true',
     },
     { 
         label: 'Phone', 
         fieldName: PHONE_FIELD.fieldApiName, 
-        type: 'phone'
-    } 
+        type: 'phone',
+        editable : 'true',
+    },
 ];
 
 export default class LeadList extends LightningElement {
 
-    columns = COLUMNS;
-    leads;
-    error;
+    @track columns = COLUMNS;
+    @track leads;
+    @track error;
+    @track draftValues = [];
+    @track wiredResult;
 
     @wire (getLeads)
-    wiredLeads ({data, error}) {
-        console.log({data});
-        console.log({error});
-        if(data) {
-            let resultData = [];
-            for (let i=0; i<data.length; i++) {
+    wiredLeads (result) {
+        this.wiredResult = result;
+        if(result.data) {
+            this.leads = [];
+            for (let i=0; i<result.data.length; i++) {
                 let row = {};
-                Object.assign(row, data[i]);
+                Object.assign(row, result.data[i]);
                 Object.assign(row, {NameLabel: row.Name});
                 Object.assign(row, {NameUrl:'/lightning/r/Lead/' +row.Id + '/view'});
-                resultData.push(row);
+                this.leads.push(row);
             }
-            this.leads = resultData;
             this.error = undefined;
-
         }
-        else if (error) {
+        else if (result.error) {
             this.error = error;
             this.leads = undefined;
         }
+    }
+
+    handleSave(event) {
+
+        const inputRecords =  event.detail.draftValues.slice().map(draft => {
+            const fields = Object.assign({}, draft);
+            return { fields };
+        });
+
+        const promises = inputRecords.map(inputRecord => updateRecord(inputRecord));
+
+        Promise.all(promises)
+        .then(() => {
+            this.dispatchEvent(
+                new ShowToastEvent({
+                    title: 'Success',
+                    message: 'All Leads updated',
+                    variant: 'success'
+                })
+            );
+            this.draftValues = [];
+            return refreshApex(this.wiredResult);
+        }).catch(error => {
+            this.dispatchEvent(
+                new ShowToastEvent({
+                    title: 'Error creating record',
+                    message: error.body.message,
+                    variant: 'error'
+                })
+            );
+        });
     }
 
 }
